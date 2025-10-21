005738c56e5fec3da3f1337b282e4dc6f2b13189
proonk <165022786+proonk@users.noreply.github.com>
2025-10-21 10:07:56 +0800

    back setup

---
+++ Lab 2/Lab 2/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Lab_2")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Lab_2.Pages")]
